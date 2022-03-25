58e7d9aa92c76d1b0ba7529d5147cdc019940184
merinsara <chinjurachel@gmail.com>
2022-03-25 14:39:38 +0530

    final commit

---
+++ app/src/main/java/com/example/mywhatsapp/TabsAccessorAdaptor.java
@@ -0,0 +1,65 @@
+package com.example.mywhatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdaptor extends FragmentPagerAdapter
+{
+    public TabsAccessorAdaptor(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i) {
+        switch(i){
+            case 0:
+                ChatsFragment chatsFragment=new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                GroupsFragment groupsFragment=new GroupsFragment();
+                return groupsFragment;
+
+            case 2:
+                ContactsFragment contactsFragment=new ContactsFragment();
+                return contactsFragment;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch(position){
+            case 0:
+                ChatsFragment chatsFragment=new ChatsFragment();
+                return "Chats";
+
+            case 1:
+                GroupsFragment groupsFragment=new GroupsFragment();
+                return "Groups";
+
+            case 2:
+                ContactsFragment contactsFragment=new ContactsFragment();
+                return "Contacts";
+
+            default:
+                return null;
+        }
+
+    }
+}
